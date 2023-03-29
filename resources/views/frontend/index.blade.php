0b2422f3bc5a8330eb61e2b01859d1052858de20
Shakil Ahmed <sa27289@gmail.com>
2023-03-29 23:05:26 +0600

    responsive

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>shakil</title>
+    <style>
+
+    .menu {
+      background-color: cadetblue;
+      display: flex;
+      justify-content: flex-end;
+    }
+    .menu a{
+      text-decoration: none;
+      padding: 20px;
+      color: white;
+    }
+    .menu a:hover{
+      background-color: grey;
+    }
+    @media screen and(max-width:600px){
+      .menu{
+          flex-direction: column;
+          padding: 10px;
+      }
+      .menu a{
+        background-color: indigo;
+        margin: 10px 0;
+        text-align: center;
+        border-radius: 10px;
+      }
+    }
+
+
+    </style>
+  </head>
+  <body>
+<div class="menu">
+  <a href="">Home</a>
+  <a href="">about us</a>
+  <a href="">tutorials</a>
+  <a href="">contact us</a>
+
+</div>
+  </body>
+</html>
